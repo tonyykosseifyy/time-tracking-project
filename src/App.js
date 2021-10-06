ed8510d82b29bdecff85d4b3d63f1061613370ce
tonyykosseifyy <tonykosseify123@gmail.com>
2021-10-06 13:40:54 +0300

    creating the app wrapper component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { AppWrapper } from './components' ;
+
 
 function App() {
   return (
-    <div className="app">
-      it is showingggg
-    </div>
+    <AppWrapper>
+      <h1>Oh it is working </h1>
+    </AppWrapper>
   );
 }
 
---
+++ src/components.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components' ;
+
+
+export const AppWrapper = styled.section`
+    background-color: var(--Very-dark-blue) ;
+    color: white ;
+    @media (min-width: 765px) {
+        display: grid ;
+        place-items: center ;
+        height: 100vh ;
+        width: 100vw ;
+    }
+`
